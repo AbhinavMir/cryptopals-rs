43737099fb9ae6c4ab6b10a84e47802a7e0bf0b6
Abhinav Srivastava <atg271@gmail.com>
2023-02-07 12:53:06 -0500

    wip

---
+++ src/basics/hex_to_base64.rs
@@ -0,0 +1,11 @@
+use base64;
+
+pub fn hex_to_base64(hex_string: &str) -> Result<String, base64::Base64Error> {
+    let bytes = hex::decode(hex_string)?;
+    Ok(base64::encode(&bytes))
+}
+
+pub fn check(){
+    // print hello
+    println!("Hello, world!");
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use basics::hex_to_base64;
+
+fn main() {
+    // let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    // let base64_string = hex_to_base64(hex_string).unwrap();
+    // println!("Hex string: {}", hex_string);
+    // println!("Base64 string: {}", base64_string);
+    hex_to_base64::check();
+}
