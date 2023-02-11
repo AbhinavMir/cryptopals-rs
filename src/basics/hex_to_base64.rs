6d7374c81d2678a612b04d5af99abc42aec4c634
Abhinav Srivastava <atg271@gmail.com>
2023-02-11 00:14:03 -0500

    wip

--- src/basics/hex_to_base64.rs
+++ src/basics/hex_to_base64.rs
@@ -1,6 +1,6 @@
 use base64;
 
-pub fn hex_to_base64(hex_string: &str) -> Result<String, base64::Base64Error> {
+pub fn hex_to_base64(hex_string: &str) -> Result<String, base64::DecodeError> {
     let bytes = hex::decode(hex_string)?;
     Ok(base64::encode(&bytes))
 }
