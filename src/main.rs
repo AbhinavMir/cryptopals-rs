f9b099bd637556e661b77c669e255c61b8d09f12
Abhinav Srivastava <atg271@gmail.com>
2023-02-11 01:39:24 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod basics;
 
 fn main() {
-    basics::hex_to_base64::check();
+    let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    basics::hex_to_base64::hex_to_base64(hex_string);
 }
