030b7284b23866989fbadbe5acd0fba015f8b266
Abhinav Srivastava <atg271@gmail.com>
2023-02-11 01:42:30 -0500

    wip

--- src/basics/hex_to_base64.rs
+++ src/basics/hex_to_base64.rs
@@ -1,14 +1,11 @@
 use base64;
 
-pub fn hex_to_base64(hex_string: &str)
-{
-    // convert hex to bytes
+pub fn hex_to_base64(hex_string: String) {
+    // convert hex string to bytes
     let bytes = hex::decode(hex_string).unwrap();
-
     // convert bytes to base64
-    let base64_string = base64::encode(&bytes);
-
-    // print base64
+    let base64_string = base64::encode(bytes);
+    // print base64 string
     println!("{}", base64_string);
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod basics;
 
 fn main() {
-    let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    // take user input in hex_string variable
+    let mut hex_string: String = String::new();
+    println!("Enter a hex string:");
+    std::io::stdin().read_line(&mut hex_string).expect("Failed to read line");
     basics::hex_to_base64::hex_to_base64(hex_string);
 }
